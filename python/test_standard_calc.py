4aa419d537ec705f0d0bcd670f27d19356400f71
Akshat-Kalra <93530371+Akshat-Kalra@users.noreply.github.com>
2023-08-22 01:32:12 +0530

    Update test_standard_calc.py for flake8 styling

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,27 +6,26 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():    # tests for 0
     assert bound_to_180(0) == 0
-    
+
 
 def test_bound_basic2():    # tests for negative angles
     assert bound_to_180(-200) == 160
-    
+
 
 def test_bound_basic3():    # tests for positive angles
     assert bound_to_180(300) == -60
-    
+
 
 def test_bound_multiples():    # tests for multiples of 360
     assert bound_to_180(720) == 0
-    
+
 
 def test_bound_boundary1():    # test the boundary cases
     assert bound_to_180(180) == -180
-    
+
 
 def test_bound_boundary2():    # test the boundary cases
     assert bound_to_180(-180) == -180
-    
 
 
 """ Tests for is_angle_between() """
